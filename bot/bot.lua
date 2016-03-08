e3f7f96af05e367f2e9b11a763bac902ae270c64
MrFarzad <botmrfarzad@gmail.com>
2016-03-08 23:49:27 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {182420116,207534428,90971889},
     admin_users = {},
     disabled_channels = {}
   }
